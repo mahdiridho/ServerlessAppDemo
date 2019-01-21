996be4c66701401cbb6899233360a47da6b5eb74
mahdiridho <mahdi.ridho@gmail.com>
2019-01-21 21:32:22 +1100

    Get AWS Credential for unauthenticated user from the webapp

--- webapp/src/app-index/app-index.js
+++ webapp/src/app-index/app-index.js
@@ -8,26 +8,44 @@ import 'aws-sdk/dist/aws-sdk.min';
 class AppIndex extends PolymerElement {
   static get template() {
     return html`
-      <style>
-        :host {
-          display: block;
-        }
-      </style>
-      <h2>Hello [[prop1]]!</h2>
+    <input id="getCred" type="button" on-click="getCredential" value="Get AWS Credential">
+    <input id="clearCred" type="button" on-click="clearCredential" value="Clear AWS Credential" style="display: none">
     `;
-  }
-  static get properties() {
-    return {
-      prop1: {
-        type: String,
-        value: 'app-index'
-      }
-    };
   }
 
   connectedCallback() {
     super.connectedCallback();
     console.log("AWS : ",AWS)
+    AWS.config.update({
+      region: "ap-southeast-1"
+    })
+  }
+
+  getCredential() {
+    if (typeof AWS == 'undefined')
+      return console.log('the AWS object is not found');
+    let cognitoParam = {
+      'IdentityPoolId': <cognito_identity_pool_id>
+    }
+
+    AWS.config.credentials=new AWS.CognitoIdentityCredentials(cognitoParam);
+    let gp=AWS.config.credentials.getPromise();
+    gp.then(()=>{
+      console.log('getCredentials done: ',AWS.config.credentials.identityId)
+      alert('AWS Credential: \n'+AWS.config.credentials.identityId)
+      this.$.getCred.style.display = "none";
+      this.$.clearCred.style.display = "block";
+    }).catch((err)=>{
+      console.log('AWS.config.credentials.get')
+      console.log("Error login : ", err);
+      this.clearCredential();
+    })
+  }
+
+  clearCredential() {
+    if (AWS.config)
+      AWS.config.credentials.clearCachedId();
+    window.location.href = "/";
   }
 }
 
