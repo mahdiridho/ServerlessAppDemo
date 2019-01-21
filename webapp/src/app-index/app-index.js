0f137f2aef0e30f036a777bdd8be7cfe1ef1ce7b
mahdiridho <mahdi.ridho@gmail.com>
2019-01-21 22:36:00 +1100

    Login App demo with cognito user pool

--- webapp/src/app-index/app-index.js
+++ webapp/src/app-index/app-index.js
@@ -1,5 +1,4 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-import 'aws-sdk/dist/aws-sdk.min';
 
 /**
  * @customElement
@@ -8,44 +7,87 @@ import 'aws-sdk/dist/aws-sdk.min';
 class AppIndex extends PolymerElement {
   static get template() {
     return html`
-    <input id="getCred" type="button" on-click="getCredential" value="Get AWS Credential">
-    <input id="clearCred" type="button" on-click="clearCredential" value="Clear AWS Credential" style="display: none">
+    <input id="signin" type="button" value="Login" on-click="login" />
+    <input id="signout" type="button" value="Logout" on-click="logout" style="display: none" />
     `;
+  }
+
+  static get properties() {
+    return {
+      token : String // auth token
+    }
   }
 
   connectedCallback() {
     super.connectedCallback();
-    console.log("AWS : ",AWS)
-    AWS.config.update({
-      region: "ap-southeast-1"
-    })
-  }
-
-  getCredential() {
-    if (typeof AWS == 'undefined')
-      return console.log('the AWS object is not found');
-    let cognitoParam = {
-      'IdentityPoolId': <cognito_identity_pool_id>
+    if (window.XMLHttpRequest) var staticVar = new XMLHttpRequest();
+    else var staticVar = new ActiveXObject('Microsoft.XMLHTTP');
+    staticVar.onreadystatechange = (e)=>{
+      if (staticVar.readyState === 4) {
+        if(staticVar.status == 200) {
+          window.Polymer = JSON.parse(staticVar.responseText);
+          window.dispatchEvent(new CustomEvent('polymerVars'));
+        } else
+          console.log("error")
+      }
     }
 
-    AWS.config.credentials=new AWS.CognitoIdentityCredentials(cognitoParam);
-    let gp=AWS.config.credentials.getPromise();
-    gp.then(()=>{
-      console.log('getCredentials done: ',AWS.config.credentials.identityId)
-      alert('AWS Credential: \n'+AWS.config.credentials.identityId)
-      this.$.getCred.style.display = "none";
-      this.$.clearCred.style.display = "block";
-    }).catch((err)=>{
-      console.log('AWS.config.credentials.get')
-      console.log("Error login : ", err);
-      this.clearCredential();
-    })
+    staticVar.open('get', "src/app-vars.json", true);
+    staticVar.send(null);
+
+    this.re = new RegExp(/^.*\//);
+    if (window.sessionStorage.getItem("token")) {
+      this.token = window.sessionStorage.getItem("token");
+      this.authenticated();
+      return;
+    }
+    let urlRefer = window.location.href.replace("#","?");
+    let url = new URL(urlRefer);
+
+    if((url.searchParams.get("id_token") || url.searchParams.get("access_token")) && url.searchParams.get("expires_in") && url.searchParams.get("token_type")) {
+      window.opener.postMessage((url.searchParams.get("id_token"))?url.searchParams.get("id_token"):url.searchParams.get("access_token"), this.re.exec(window.location.href)[0]);
+      window.close();
+    }
   }
 
-  clearCredential() {
-    if (AWS.config)
-      AWS.config.credentials.clearCachedId();
+  /** Once click login event, Open popup login redirect to User Pool Domain Name
+  Initialize a cognito auth object.
+  */
+  login(){
+    if (window.Polymer.clientId && window.Polymer.poolName && window.Polymer.userPoolId) {
+      console.log('login')
+      let w = window.open('https://'+window.Polymer.poolName+'.auth.'+window.Polymer.userPoolId.split("_")[0]+'.amazoncognito.com/login?response_type=token&client_id='+window.Polymer.clientId+'&redirect_uri='+this.re.exec(window.location.href)[0]+'&scope=email+profile+openid','popup','width=600,height=600');
+      w.document.title = "User Authentication";
+      window.addEventListener("message", (e)=>{
+        this.token = e.data;
+        window.sessionStorage.setItem("token",e.data);
+        if (e.origin == window.location.origin) {
+          this.authenticated();
+        }
+      }, false);
+      window.addEventListener("error", (e)=>{
+        console.log(e)
+      }, false);
+    } else
+      alert("Missing properties!")
+  }
+
+  /** Logout of app. Clear any session storage
+  */
+  logout(){
+    console.log('logout')
+    window.sessionStorage.removeItem("token");
     window.location.href = "/";
+  }
+
+  /** Overload this to handle higher level setup in your class.
+  On successful authentication call this method
+  */
+  authenticated(){
+    console.log(this.token)
+    console.log('authenticated');
+    this.$.signin.style.display = "none";
+    this.$.signout.style.display = "block";
   }
 }
 
